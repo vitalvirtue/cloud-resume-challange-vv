420e1f3c0429984a08623190876979007d076a2d
vitalvirtue <hyterdem@hotmail.com>
2023-02-02 14:12:39 +0300

    index.html

--- PutFunction/test.py
+++ PutFunction/test.py
@@ -2,14 +2,13 @@
 import unittest
 import boto3
 import json
-
-from app import lambda_handler
+import app
 
 
 class TestAPI(unittest.TestCase):
     def test_putApi_works(self):
 
-        result = lambda_handler(0,0)
+        result = app.lambda_handler(0,0)
         self.assertEqual(result['statusCode'], 200)
 
 if __name__ == '__main__':
