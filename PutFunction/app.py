a2034277ded7797c6a5eb8ecb48ca92396020e54
vitalvirtue <hyterdem@hotmail.com>
2023-02-03 00:34:16 +0300

    update app.py

--- PutFunction/app.py
+++ PutFunction/app.py
@@ -24,9 +24,7 @@ def lambda_handler(event, context):
     if 'Item' in response:
         response = table.update_item(
              Key={
-                'ID': {
-                    'S' : 'visitors'
-                }
+                'ID': 'visitors'
             },
             UpdateExpression="set total_count = total_count + :N",
             ExpressionAttributeValues={
